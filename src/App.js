a6c1cb7c469bccb969cf6eeba8a3cbba8dfc81c1
Junobee25 <wnsdhqo@naver.com>
2023-04-11 00:04:17 +0900

    DiaryList 렌더링추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+
+const dummyList = [
+  {
+    id : 1,
+    author : "배준오",
+    content : "하이 1",
+    emotion : 5,
+    created_date : new Date().getTime()
+  },
+  {
+    id : 2,
+    author : "홍길동",
+    content : "하이 2",
+    emotion : 3,
+    created_date : new Date().getTime()
+  },
+  {
+    id : 3,
+    author : "아무개",
+    content : "하이 3",
+    emotion : 2,
+    created_date : new Date().getTime()
+  },
+  {
+    id : 4,
+    author : "배민서",
+    content : "하이 4",
+    emotion : 4,
+    created_date : new Date().getTime()
+  },
+  
+]
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor></DiaryEditor>
+      <DiaryList diaryList={dummyList}></DiaryList>
     </div>
   );
 }
