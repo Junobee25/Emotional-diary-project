75422c35e49cdfba409fe808a16b6d1a4e1938fe
Junobee25 <wnsdhqo@naver.com>
2023-05-18 00:40:13 +0900

    useMemo optimization

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useMemo,useEffect, useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
@@ -84,9 +84,27 @@ function App() {
     )
   }
 
+  const getDiaryAnalysis = useMemo(
+    () =>
+    {
+    console.log("일기 분석 시작");
+
+    const goodCount = data.filter((it)=> it.emotion >=3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100
+    return {goodCount,badCount,goodRatio};
+  },[data.length]
+  );
+
+  const {goodCount,badCount,goodRatio} = getDiaryAnalysis;
+
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate}></DiaryEditor>
+      <div>전체 일기 : {data.length} </div>
+      <div>기분 좋음 : {goodCount} </div>
+      <div>기분 나쁨 : {badCount} </div>
+      <div>기분 좋은 일기 비율 : {goodRatio} </div>
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data}></DiaryList>
     </div>
   );
